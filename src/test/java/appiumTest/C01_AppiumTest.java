1a808e4521d303594dab573ee52023ed56bbd10e
Mete <hasanozyer06@gmail.com>
2022-09-13 22:12:59 +0300

    Creating appium executing it

--- src/test/java/appiumTest/C01_AppiumTest.java
+++ src/test/java/appiumTest/C01_AppiumTest.java
@@ -1,39 +1,59 @@
 package appiumTest;
 
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.touch.offset.PointOption;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.concurrent.TimeUnit;
 
 public class C01_AppiumTest {
     @Test
-    public void test01() throws MalformedURLException {
+    public void test01() throws MalformedURLException, InterruptedException {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel3");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel2");
         //todo emulator -list-avds => cihaz ismini de bu kod sayesinde buluruz.
-        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "7.0");
         //todo adb shell getprop ro.build.version.release => version bulmak için
         desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
 
-        desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\PC\\IdeaProjectsU\\com.GuiderSoftAppium\\src\\app\\Duolingo.apk");
+        desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\PC\\IdeaProjects\\com.GuiderSoftAppium\\src\\app\\Duolingo.apk");
 
         AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http:127.0.0.1:4723/wd/hub"), desiredCapabilities);
+        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+
 
         WebElement getStart = driver.findElement(By.id("com.duolingo:id/introFlowNewUserButton"));
         String getaText = getStart.getText();
         System.out.println("getaText = " + getaText);
         getStart.click();
 
+
         WebElement english = driver.findElement(By.xpath("//*[@text=\"English\"]"));
         english.click();
+
+        TouchAction touchAction = new TouchAction(driver);
+        touchAction.longPress(PointOption.point(843, 1645))
+                .moveTo(PointOption.point(868, 466))
+                .release()
+                .perform();
+/*(new TouchAction(driver))
+  .press(PointOption.point(843, 1645}))
+  .moveTo(PointOption.point(868, 466}))
+  .release()
+  .perform();*/
+
 
         WebElement turksh = driver.findElement(By.xpath("//*[@text=\"Turkish\"]"));
         turksh.click();
@@ -48,7 +68,7 @@ public class C01_AppiumTest {
         ciddi.click();
 
         driver.findElement(By.id("com.duolingo:id/xpGoalContinueButton")).click();
-        driver.findElement(By.id("driver.findElement(By.id(\"\"))\n")).click();
+        driver.findElement(By.id("com.duolingo:id/placementHeader")).click();
 
         WebElement testBasla = driver.findElement(By.id("com.duolingo:id/primaryButton"));
         Assert.assertTrue(testBasla.isDisplayed());

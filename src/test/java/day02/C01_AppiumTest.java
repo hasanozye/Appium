7cce25b62069a92c1d9bb4ae6e89ffb1e0488522
Mete <hasanozyer06@gmail.com>
2022-09-14 00:20:53 +0300

    Creating appium executing it

--- src/test/java/day02/C01_AppiumTest.java
+++ src/test/java/day02/C01_AppiumTest.java
@@ -1,4 +1,4 @@
-package appiumTest;
+package day02;
 
 import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
@@ -19,47 +19,41 @@ import java.util.concurrent.TimeUnit;
 
 public class C01_AppiumTest {
     @Test
-    public void test01() throws MalformedURLException, InterruptedException {
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel2");
-        //todo emulator -list-avds => cihaz ismini de bu kod sayesinde buluruz.
-        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "7.0");
-        //todo adb shell getprop ro.build.version.release => version bulmak için
-        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+    public void testAppium01() throws MalformedURLException {
 
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel3");
+        // cmd'ye emulator -list-avds => cihaz ismini bu kod sayesinde buluruz.
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
+        // cmd'ye adb shell getprop ro.build.version.release => version bulmak için
+        // cmd'ye adb shell  settings get secure android_id => cihaz id'si için
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
         desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\PC\\IdeaProjects\\com.GuiderSoftAppium\\src\\app\\Duolingo.apk");
 
-        AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http:127.0.0.1:4723/wd/hub"), desiredCapabilities);
+
+        AndroidDriver<AndroidElement> driver = new AndroidDriver<>(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
         driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
 
-
-        WebElement getStart = driver.findElement(By.id("com.duolingo:id/introFlowNewUserButton"));
-        String getaText = getStart.getText();
-        System.out.println("getaText = " + getaText);
-        getStart.click();
-
+        WebElement getStarted = driver.findElement(By.id("com.duolingo:id/introFlowNewUserButton"));
+        System.out.println("Get Started Element Text : " + getStarted.getText());
+        getStarted.click();
 
         WebElement english = driver.findElement(By.xpath("//*[@text=\"English\"]"));
         english.click();
 
         TouchAction touchAction = new TouchAction(driver);
-        touchAction.longPress(PointOption.point(843, 1645))
-                .moveTo(PointOption.point(868, 466))
+        touchAction.longPress(PointOption.point(881, 1870))
+                .moveTo(PointOption.point(874, 230))
                 .release()
                 .perform();
-/*(new TouchAction(driver))
-  .press(PointOption.point(843, 1645}))
-  .moveTo(PointOption.point(868, 466}))
-  .release()
-  .perform();*/
 
+        WebElement turkish = driver.findElement(By.xpath("//*[@text=\"Turkish\"]"));
+        turkish.click();
 
-        WebElement turksh = driver.findElement(By.xpath("//*[@text=\"Turkish\"]"));
-        turksh.click();
-
-        WebElement willOk = driver.findElement(By.id("android:id/button1"));
-        willOk.click();
+        WebElement okButton = driver.findElement(By.id("android:id/button1"));
+        okButton.click();
 
         WebElement okul = driver.findElement(By.xpath("//android.widget.LinearLayout[@content-desc=\"school\"]/android.widget.TextView"));
         okul.click();
@@ -77,7 +71,6 @@ public class C01_AppiumTest {
 
         WebElement skip = driver.findElement(By.id("com.duolingo:id/skipButton"));
         Assert.assertTrue(skip.isDisplayed());
-
 
     }
 
